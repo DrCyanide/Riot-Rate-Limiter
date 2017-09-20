ff59f61fbcbc47dee61fd482d3a187e40c2640f4
Travis Smith <flagezooke@gmail.com>
2017-09-20 01:10:17 -0600

    Finished Limit test cases

--- tests.py
+++ tests.py
@@ -13,7 +13,8 @@ server_connection = 'http://'
 class TestLimit(unittest.TestCase):
     def setUp(self):
         self.seconds = 0.1
-        self.limit = Limit(seconds=self.seconds, limit=1, used=0)
+        self.limit_count = 1
+        self.limit = Limit(seconds=self.seconds, limit=self.limit_count, used=0)
         
     def test_defaultInit(self):
         self.limit = Limit()
@@ -58,10 +59,35 @@ class TestLimit(unittest.TestCase):
         self.assertTrue(type(used) == int)
         
     def test_setLimit(self):
-        pass
+        self.assertTrue(self.limit.limit == self.limit_count)
+        self.limit.setLimit(seconds=10, limit=100)
+        self.assertTrue(self.limit.limit == 100)
+        self.assertTrue(self.limit.seconds == 10)
+        self.assertTrue(self.limit.used == 0)
+        
+        self.limit.use()
+        
+        self.limit.setLimit(seconds=1, limit=90)
+        self.assertTrue(self.limit.limit == 90)
+        self.assertTrue(self.limit.seconds == 1)
+        self.assertTrue(self.limit.used == 1)
         
     def test_setUsed(self):
-        pass
+        self.assertTrue(self.limit.used == 0)
+        self.assertTrue(self.limit.ready())
+        self.limit.setUsed(1)
+        self.assertTrue(self.limit.used == 1)
+        self.assertFalse(self.limit.ready())
+        
+    def test_getResetTime(self):
+        self.limit.use()
+        self.assertTrue(self.limit.getResetTime() > time.time())
+        time.sleep(self.limit.seconds)
+        self.assertFalse(self.limit.getResetTime() > time.time()) # still gives when it reset, which was in the past
+        self.assertTrue(self.limit.ready())
+        self.limit.use()
+        self.assertTrue(self.limit.getResetTime() > time.time())
+        
         
 # Endpoint
 # Platform
